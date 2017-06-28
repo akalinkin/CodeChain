7e474e7ffca7bce2cd4bf38e7d7c74ce598fe0e9
Alex Kalinkin <alex@kalinkin.info>
2017-06-28 17:03:25 +0300

    Initial Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CodeChain app started");
+        }
+    }
+}
