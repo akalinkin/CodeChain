bc1e5321a517bb110e75a811cc8d0a40d949322a
Alex Kalinkin <alex@kalinkin.info>
2017-06-29 18:50:09 +0300

    Added few models

--- Models/Block.cs
+++ Models/Block.cs
@@ -1,12 +1,19 @@
+using System;
+
 namespace CodeChain.Models {
     /// Block of the chain
     public class Block {
         /// Number of block
         public ulong Number;
-
+        /// Parent block hash
+        public byte[] ParentHash;
         /// Current block hash
         public byte[] Hash;
         /// Block data
         public byte[] Data;
+        /// Block timestamp UTC
+        public DateTime Time;
+        /// Block owner address - Node address which produce block
+        public Address Owner;
     }
 }
---
+++ Models/Node.cs
@@ -0,0 +1,5 @@
+namespace CodeChain.Models {
+    public class Node {
+        
+    }
+}
